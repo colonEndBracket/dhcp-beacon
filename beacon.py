da2199e45df3fce91545e569981772e9a01a7945
Eric Iniguez <43428465+colonEndBracket@users.noreply.github.com>
2021-06-21 09:56:14 -0500

    initial commit

---
+++ beacon.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import smtplib
+from datetime import datetime
+from email.message import EmailMessage
+from netifaces import interfaces, ifaddresses, AF_INET
+
+from creds import str_username, str_password, list_recipients
+
+def main():
+    # Subject
+    now = datetime.now().date()
+    str_subject = '['+str(now)+'] '+'IP Update'
+
+    # Message
+    str_content = ""
+    for ifaceName in interfaces():
+        # Build list matching interface name to ip addresses
+        addresses = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr':'No IP addr'}] )]
+        str_content += str(ifaceName+' - '+', '.join(addresses)+'\n')
+    print(str_content)
+
+    # Sending Email
+    sendEmail(list_recipients,str_subject,str_content)
+
+def sendEmail(list_recipients,str_subject,str_content):
+    msg = EmailMessage()
+    msg['Subject'] = str_subject
+    msg.set_content(str_content)
+
+    # Logistics
+    try:
+        server = smtplib.SMTP('smtp.gmail.com',587)
+        server.ehlo()
+        server.starttls()
+    except:
+        print("Issue connecting to Gmail SMTP")
+    server.login(str_username,str_password)
+    server.sendmail(str_username, list_recipients, str(msg))
+    server.quit()
+    
+if __name__ == "__main__":
+    main()
